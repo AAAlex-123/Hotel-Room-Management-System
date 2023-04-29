c6f1d7d88592e6e8cf0d71504f9ded58bf2ec6bd
Alex Mandelias <alexmandelias@gmail.com>
2023-04-29 22:37:43 +0300

    Implements the front end of the Login Feature
    TODO:
    - navigation
    - integration with the rest of the app

---
+++ src/Housekeeping/feature/login/src/main/java/alexman/hrms/feature/login/LoginScreen.kt
@@ -0,0 +1,134 @@
+package alexman.hrms.feature.login
+
+import alexman.hrms.core.data.repository.fake.FakeCleaningStaffRepository
+import alexman.hrms.core.designsystem.PreviewBoth
+import alexman.hrms.core.designsystem.component.MediumDisplayText
+import alexman.hrms.core.designsystem.component.HousekeepingTopAppBar
+import alexman.hrms.core.designsystem.component.InputField
+import alexman.hrms.core.designsystem.component.ButtonWithText
+import alexman.hrms.core.designsystem.component.ErrorLabel
+import alexman.hrms.core.designsystem.theme.HousekeepingTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+@PreviewBoth
+@Composable
+fun LoginScreenPreview() {
+    HousekeepingTheme {
+        LoginScreen(
+            LoginViewModel(FakeCleaningStaffRepository()),
+            onNavigateToHome = { _: String, _: String -> }
+        )
+    }
+}
+
+@Composable
+fun LoginScreen(
+    loginViewModel: LoginViewModel,
+    onNavigateToHome: (String, String) -> Unit,
+) {
+    suspend fun authenticate(username: String, password: String): String? =
+        withContext(Dispatchers.IO) {
+            return@withContext loginViewModel.authenticate(username, password)
+        }
+
+    LoginScreenContent(
+        onClick = ::authenticate,
+        onNavigateToHome = onNavigateToHome,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreenContent(
+    onClick: suspend (String, String) -> String?,
+    onNavigateToHome: (String, String) -> Unit,
+) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    var success by remember { mutableStateOf(true) }
+
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold (
+        topBar = { HousekeepingTopAppBar(text = "Login") },
+    ) {
+        paddingValues ->
+            Surface (
+                modifier = Modifier
+                    .padding(paddingValues)
+            ) {
+                Column {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.spacedBy(
+                            48.dp,
+                            Alignment.CenterVertically
+                        ),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                        ) {
+                            MediumDisplayText(text = "Staff ID")
+                            InputField(
+                                value = username,
+                                onValueChange = { username = it },
+                                placeholderText = "Staff ID",
+                                isError = !success,
+                            )
+                        }
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                        ) {
+                            MediumDisplayText(text = "Password")
+                            InputField(
+                                value = password,
+                                onValueChange = { password = it },
+                                placeholderText = "Password",
+                                isError = !success,
+                            )
+                        }
+                        ButtonWithText(
+                            text = "Submit",
+                            onClick = {
+                                coroutineScope.launch {
+                                    val sessionId = onClick(username, password)
+
+                                    success = sessionId !== null
+
+                                    if (sessionId !== null) {
+                                        onNavigateToHome(username, sessionId)
+                                    }
+                                }
+                            },
+                        )
+                        if (!success) {
+                            ErrorLabel(
+                                text = "Username and Password don't match!",
+                            )
+                        }
+                    }
+                }
+            }
+    }
+}
---
+++ src/Housekeeping/feature/login/src/main/java/alexman/hrms/feature/login/LoginViewModel.kt
@@ -0,0 +1,17 @@
+package alexman.hrms.feature.login
+
+import alexman.hrms.core.data.repository.AuthenticationQuery
+import alexman.hrms.core.data.repository.CleaningStaffRepository
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel(
+    private val cleaningStaffRepository: CleaningStaffRepository,
+) : ViewModel() {
+    suspend fun authenticate(login: String, password: String): String? =
+        cleaningStaffRepository.authenticate(
+            AuthenticationQuery(
+                login = login,
+                password = password,
+            ),
+        )
+}
