78852418c14bc4f37604e0bc3ee11f9c872c213e
Alex Mandelias <alexmandelias@gmail.com>
2023-04-30 22:47:04 +0300

    Implements the front end of the Home feature
    TODO:
    - navigation
    - integration with the rest of the app

---
+++ src/Housekeeping/feature/home/src/main/java/alexman/hrms/feature/home/HomeScreen.kt
@@ -0,0 +1,74 @@
+package alexman.hrms.feature.home
+
+import alexman.hrms.core.data.repository.fake.FakeCleaningStaffRepository
+import alexman.hrms.core.designsystem.PreviewLight
+import alexman.hrms.core.designsystem.component.ButtonWithIcon
+import alexman.hrms.core.designsystem.component.HousekeepingTopAppBar
+import alexman.hrms.core.designsystem.component.LargeDisplayText
+import alexman.hrms.core.designsystem.theme.HousekeepingTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@PreviewLight
+@Composable
+fun HomeScreenPreview() {
+    HousekeepingTheme {
+        HomeScreen(
+            HomeViewModel(2, FakeCleaningStaffRepository()),
+            onNavigateToLogIn = { },
+        )
+    }
+}
+
+@Composable
+fun HomeScreen(
+    homeViewModel: HomeViewModel,
+    onNavigateToLogIn: () -> Unit
+) {
+    val (staffName, staffId) = homeViewModel.uiState
+
+    HomeScreenContent(staffName, staffId, onNavigateToLogIn)
+}
+
+@Composable
+private fun HomeScreenContent(
+    staffName: String,
+    staffId: Int,
+    onNavigateToLogIn: () -> Unit,
+) {
+    Surface {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            HousekeepingTopAppBar(
+                text = staffId.toString(),
+                actions = { LogoutButton (onClick = onNavigateToLogIn) }
+            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                LargeDisplayText("Staff ID: $staffId")
+                LargeDisplayText("Name: $staffName")
+            }
+        }
+    }
+}
+
+@Composable
+private fun LogoutButton(onClick: () -> Unit) {
+    ButtonWithIcon(
+        id = R.drawable.ic_menu_logout,
+        alt = "logout",
+        onClick = onClick,
+    )
+}
---
+++ src/Housekeeping/feature/home/src/main/java/alexman/hrms/feature/home/HomeViewModel.kt
@@ -0,0 +1,39 @@
+package alexman.hrms.feature.home
+
+import alexman.hrms.core.data.repository.CleaningStaffQuery
+import alexman.hrms.core.data.repository.CleaningStaffRepository
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+data class HomeUiState(
+    val staffName: String,
+    val staffId: Int,
+)
+
+class HomeViewModel(
+    cleaningStaffId: Int,
+    cleaningStaffRepository: CleaningStaffRepository,
+) : ViewModel() {
+
+    var uiState: HomeUiState by mutableStateOf(HomeUiState("", 0))
+        private set
+
+    init {
+        viewModelScope.launch {
+            with (
+                cleaningStaffRepository.getCleaningStaff(
+                    CleaningStaffQuery(cleaningStaffId = cleaningStaffId)
+                )
+            ) {
+                uiState = uiState.copy(
+                    staffName = this.name,
+                    staffId = this.id,
+                )
+            }
+        }
+    }
+}
