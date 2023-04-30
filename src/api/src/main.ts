498b84bf7d1ab20798e811a29c68148020376f58
Anastasis575 <79604688+Anastasis575@users.noreply.github.com>
2023-04-30 15:22:48 +0300

    added needed files

--- src/api/src/main.ts
+++ src/api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Hestia api')
+    .setDescription('The Hestia API description')
+    .setVersion('1.0')
+    .addTag('hotel')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
