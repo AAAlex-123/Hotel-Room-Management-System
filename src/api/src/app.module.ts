7aa92d59c868f3e87bf4230a73924193078303de
Anastasis575 <79604688+Anastasis575@users.noreply.github.com>
2023-05-09 20:47:56 +0300

    attempt to fix docker

--- src/api/src/app.module.ts
+++ src/api/src/app.module.ts
@@ -4,12 +4,15 @@ import { PrismaService } from './prisma/prisma.service';
 import { RoomController } from './room/room.controller';
 import { EmployeeController } from './employee/employee.controller';
 import { ReservationController } from './reservation/reservation.controller';
-import { HousekeeperController } from './housekeeper/housekeeper.controller';
-import { ChambermaidController } from './chambermaid/chambermaid.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, RoomController, EmployeeController, ReservationController, HousekeeperController, ChambermaidController],
+  controllers: [
+    AppController,
+    RoomController,
+    EmployeeController,
+    ReservationController,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
