68f00eb655de64a020d2bdc6112e733fbbd380be
Anastasis575 <79604688+Anastasis575@users.noreply.github.com>
2023-05-09 19:39:10 +0300

    updated la schema

--- src/api/src/reservation/reservation.client.entity/reservation.client.entity.spec.ts
+++ src/api/src/reservation/reservation.client.entity/reservation.client.entity.spec.ts
@@ -2,6 +2,9 @@ import { ReservationClientEntity } from './reservation.client.entity';
 
 describe('ReservationClientEntity', () => {
   it('should be defined', () => {
-    expect(new ReservationClientEntity()).toBeDefined();
+    const it = new ReservationClientEntity();
+    it.room_number = '001';
+    expect(it.room_number).toBeDefined();
+    expect(it.room_number).toEqual('001');
   });
 });
