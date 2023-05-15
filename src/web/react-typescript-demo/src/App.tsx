b65ae9c7895eb53d551c891a70713777c476eb23
ElectraFabCap <electra.papadopoulou2001@gmail.com>
2023-05-15 23:27:21 +0300

    semi-functional login, the ui bad

--- src/web/react-typescript-demo/src/App.tsx
+++ src/web/react-typescript-demo/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import Login from './Components/Login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainScreen from './Pages/MainScreen';
 
 function App() {
   return (
     <div className="App">
-
-    <Login/>
+      <Router> 
+        <Routes>
+          <Route path="/main" element={<MainScreen/>}/> 
+          <Route path="/" element={<Login/>}/> 
+        </Routes>
+        </Router> 
     </div>
   );
 }
---
+++ src/web/react-typescript-demo/src/Pages/MainScreen.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+export default function MainScreen () {
+    return (
+      <div>MainScreen</div>
+    )
+  }
+
