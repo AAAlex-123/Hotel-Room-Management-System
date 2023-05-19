e57ae7a87a83a339545d67eaceae2c6c3854126d
Alex Mandelias <alexmandelias@gmail.com>
2023-05-19 21:32:44 +0300

    Cleans up source files in the :core:model module

--- src/Housekeeping/core/model/src/main/java/alexman/hrms/core/model/data/CleaningStaff.kt
+++ src/Housekeeping/core/model/src/main/java/alexman/hrms/core/model/data/CleaningStaff.kt
@@ -9,24 +9,3 @@ class CleaningStaff(
     val name: String,
     val cleaningStaffType: CleaningStaffType,
 )
-
-// TODO("figure out the hierarchy, if any")
-
-/*
-data class Housekeeper(
-    override val id: Int,
-    override val employeeId: Int,
-    override val name: String,
-    override val cleaningStaffType: CleaningStaffType,
-    // TODO(add the rest)
-) : CleaningStaff(id, employeeId, name, cleaningStaffType)
-
-data class CleaningLady(
-    override val id: Int,
-    override val employeeId: Int,
-    override val name: String,
-    override val cleaningStaffType: CleaningStaffType,
-    // TODO(add the rest)
-    // val rooms: Map<Int, Room>,
-) : CleaningStaff(id, employeeId, name, cleaningStaffType)
-*/
