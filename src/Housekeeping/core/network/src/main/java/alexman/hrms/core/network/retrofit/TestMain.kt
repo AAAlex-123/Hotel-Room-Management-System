7820dcf94a85ac0a80f973398e269e1da9f7a118
Alex Mandelias <alexmandelias@gmail.com>
2023-06-03 23:21:34 +0300

    Updates Test Main
    Test Main now does a full integration test between Retrofit and the
    external data source.

--- src/Housekeeping/core/network/src/main/java/alexman/hrms/core/network/retrofit/TestMain.kt
+++ src/Housekeeping/core/network/src/main/java/alexman/hrms/core/network/retrofit/TestMain.kt
@@ -2,24 +2,77 @@ package alexman.hrms.core.network.retrofit
 
 import alexman.hrms.core.network.HrmsNetworkResponse
 import alexman.hrms.core.network.model.UpstreamNetworkCleaningStaffAuth
+import alexman.hrms.core.network.model.UpstreamNetworkNoteDetails
 import alexman.hrms.core.network.model.UpstreamNetworkOrderDetails
+import alexman.hrms.core.network.model.UpstreamNetworkOrderUpdateDetails
+import alexman.hrms.core.network.model.UpstreamNetworkRoomUpdateDetails
 import kotlinx.coroutines.runBlocking
 
 // TODO("remove when done")
 private fun main() {
-    val retrofitDS = HrmsRetrofitNetworkDataSource()
+    with (HrmsRetrofitNetworkDataSource()) {
+        val roomId = "1"
+        val cleaningStaffIdWithProvisions = 6
 
-    doPrintResponse("GET /auth") { retrofitDS.authenticate(UpstreamNetworkCleaningStaffAuth("login1", "password1")) }
-    doPrintResponse("GET /employee/{id}") { retrofitDS.getCleaningStaff(1) }
-    doPrintResponse("GET /provision/{emp_id}") { retrofitDS.getOrders(2) }
-    doPrintResponse("POST /provision") { retrofitDS.placeOrder(UpstreamNetworkOrderDetails(3, "orderData3")) }
-    doPrintResponse("DELETE /provision/{id}") { retrofitDS.deleteOrder(4) }
+        var housekeeperId = req(http = "POST /auth") { authenticate(
+            UpstreamNetworkCleaningStaffAuth(
+                login = "username1",
+                password = "login",
+            )
+        ) }.body!!
+        req(http = "GET /employee/{id}") { getCleaningStaff(cleaningStaffId = housekeeperId) }
+        req(http = "GET /chambermaid") { getCleaningLadies(housekeeperId = housekeeperId) }
+        req(http = "GET /provision") { getOrders(cleaningLadyId = cleaningStaffIdWithProvisions) }
+        var newOrderId = req(http = "POST /provision") { placeOrder(
+            UpstreamNetworkOrderDetails(
+                cleaningLadyId = cleaningStaffIdWithProvisions,
+                orderData = "ORDER DATA",
+            )
+        ) }.body!!.id
+        req(http = "PUT /provision/{id}") { updateOrderState(
+            UpstreamNetworkOrderUpdateDetails(
+                id = newOrderId,
+                completed = true,
+            )
+        ) }
+        var orderIdToDelete = req(http = "POST /provision") { placeOrder(
+            UpstreamNetworkOrderDetails(
+                cleaningLadyId = cleaningStaffIdWithProvisions,
+                orderData = "ORDER DATA TO DELETE",
+            )
+        ) }.body!!.id
+        req(http = "DELETE /provision/{id}") { deleteOrder(orderId = orderIdToDelete) }
+        req(http = "GET /room") { getRooms(cleaningStaffId = housekeeperId) }
+        req(http = "GET /room/{id}") { getSingleRoom(roomId = roomId) }
+        req(http = "PUT /room/{id}") { updateRoomState(
+            UpstreamNetworkRoomUpdateDetails(
+                id = roomId,
+                cleanState = "DIRTY",
+            )
+        ) }
+        req(http = "GET /note") { getNotes(roomId = roomId) }
+        req(http = "POST /note") { addNote(
+            UpstreamNetworkNoteDetails(
+                roomId = roomId,
+                cleaningStaffId = housekeeperId,
+                noteData = "NOTE DATA",
+            )
+        ) }.body!!.id
+        var newNoteIdToDelete = req(http = "POST /note") { addNote(
+            UpstreamNetworkNoteDetails(
+                roomId = roomId,
+                cleaningStaffId = housekeeperId,
+                noteData = "NOTE DATA TO DELETE",
+            )
+        ) }.body!!.id
+        req(http = "DELETE /note/{id}") { deleteNote(noteId = newNoteIdToDelete) }
+    }
 }
 
-private fun <T> doPrintResponse(rest: String, request: suspend () -> HrmsNetworkResponse<T>) {
-    runBlocking {
+private fun <T> req(http: String, request: suspend () -> HrmsNetworkResponse<T>): HrmsNetworkResponse<T> {
+    return runBlocking {
         request()
     }.also {
-        print("\n\n---\nHTTP ${rest}\nCode: ${it.code} (OK: ${it.ok})\nBody: ${it.body}\nErrorBody: ${it.errorBody}")
+        print("\n\n---\nHTTP ${http}\nCode: ${it.code} (OK: ${it.ok})\nBody: ${it.body}\nErrorBody: ${it.errorBody}")
     }
 }
