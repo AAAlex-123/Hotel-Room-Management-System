5a7e2b9f5cf521cea8408589b0549f33b8ad7173
Alex Mandelias <alexmandelias@gmail.com>
2023-06-07 16:27:05 +0300

    Adds a filter in the Order Screen
    Orders can now be filtered to selectively show and hide the pending and
    completed orders.

--- src/Housekeeping/feature/order/src/main/java/alexman/hrms/feature/order/OrderScreen.kt
+++ src/Housekeeping/feature/order/src/main/java/alexman/hrms/feature/order/OrderScreen.kt
@@ -7,6 +7,7 @@ import alexman.hrms.core.designsystem.component.ButtonWithText
 import alexman.hrms.core.designsystem.component.HrmsPopup
 import alexman.hrms.core.designsystem.component.HrmsScaffold
 import alexman.hrms.core.designsystem.component.IconClickable
+import alexman.hrms.core.designsystem.component.LargeBodyText
 import alexman.hrms.core.designsystem.component.ListItem
 import alexman.hrms.core.designsystem.component.MediumDisplayText
 import alexman.hrms.core.designsystem.component.ScaffoldNavigation
@@ -17,19 +18,28 @@ import alexman.hrms.core.model.data.Order
 import alexman.hrms.core.model.data.OrderStatus
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalMinimumInteractiveComponentEnforcement
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.ModifierLocalReadScope
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -52,6 +62,19 @@ private fun OrderScreenContentPreview() {
             onSubmitNewOrder = { },
             onNavigateBack = { },
             scaffoldNavigation = ScaffoldNavigation(),
+        )
+    }
+}
+
+@PreviewLight
+@Composable
+private fun FilterOrdersPopupContentPreview() {
+    HrmsTheme {
+        FilterOrdersPopupContent(
+            showPending = true,
+            onShowPendingChanged = { },
+            showCompleted = false,
+            onShowCompletedChanged = { },
         )
     }
 }
@@ -109,10 +132,19 @@ private fun OrderScreenContent(
     onNavigateBack: () -> Unit,
     scaffoldNavigation: ScaffoldNavigation,
 ) {
+    var showPending by rememberSaveable { mutableStateOf(true) }
+    var showCompleted by rememberSaveable { mutableStateOf(true) }
+
     HrmsScaffold(
         topBarText = "Orders",
         onNavigationIconClick = { onNavigateBack() },
         actions = {
+            FilterOrdersButton(
+                showPending = showPending,
+                onShowPendingChanged = { showPending = it },
+                showCompleted = showCompleted,
+                onShowCompletedChanged = { showCompleted = it }
+            )
             if (staff.staffType == CleaningStaffType.CLEANING_LADY) {
                 NewOrderButton(onSubmitNewOrder)
             }
@@ -126,7 +158,10 @@ private fun OrderScreenContent(
                 .fillMaxSize()
                 .padding(16.dp),
         ) {
-            items(orders) {
+            items(orders.filter {
+                (showPending && it.completed == OrderStatus.PENDING)
+                        || (showCompleted && it.completed == OrderStatus.COMPLETED)
+            }) {
                 ListItem(
                     id = it.id,
                     text = it.orderData,
@@ -140,6 +175,90 @@ private fun OrderScreenContent(
                     },
                     SizeVariation.PRIMARY,
                 )
+            }
+        }
+    }
+}
+
+@Composable
+private fun FilterOrdersButton(
+    showPending: Boolean,
+    onShowPendingChanged: (Boolean) -> Unit,
+    showCompleted: Boolean,
+    onShowCompletedChanged: (Boolean) -> Unit,
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    IconClickable(
+        id = R.drawable.ic_menu_filter,
+        alt = "Filter Orders",
+        onClick = { expanded = true },
+        sizeVariation = SizeVariation.PRIMARY,
+        modifier = Modifier
+            .padding(8.dp),
+    )
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = { expanded = false },
+        offset = DpOffset(0.dp, 0.dp),
+    ) {
+        FilterOrdersPopupContent(
+            showPending = showPending,
+            onShowPendingChanged = onShowPendingChanged,
+            showCompleted = showCompleted,
+            onShowCompletedChanged = onShowCompletedChanged,
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun FilterOrdersPopupContent(
+    showPending: Boolean,
+    onShowPendingChanged: (Boolean) -> Unit,
+    showCompleted: Boolean,
+    onShowCompletedChanged: (Boolean) -> Unit,
+) {
+    HrmsPopup {
+        Column(
+            horizontalAlignment = Alignment.Start, // left-align checkboxes, manually center title
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier
+                .padding(16.dp),
+        ) {
+            MediumDisplayText(
+                text = "Filter Orders",
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start),
+                modifier = Modifier
+                    .padding(8.dp)
+            ) {
+                // provide custom padding
+                CompositionLocalProvider(LocalMinimumInteractiveComponentEnforcement provides false) {
+                    Checkbox(
+                        checked = showPending,
+                        onCheckedChange = onShowPendingChanged,
+                    )
+                }
+                LargeBodyText(text = "Pending")
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start),
+                modifier = Modifier
+                    .padding(8.dp)
+            ) {
+                // provide custom padding
+                CompositionLocalProvider(LocalMinimumInteractiveComponentEnforcement provides false) {
+                    Checkbox(
+                        checked = showCompleted,
+                        onCheckedChange = onShowCompletedChanged,
+                    )
+                }
+                LargeBodyText(text = "Completed")
             }
         }
     }
@@ -162,7 +281,6 @@ private fun NewOrderButton(
     DropdownMenu(
         expanded = expanded,
         onDismissRequest = { expanded = false },
-        modifier = Modifier,
         offset = DpOffset(0.dp, 0.dp),
         // properties = PopupProperties, // defaults are fine
     ) {
